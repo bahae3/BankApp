53c7f49930207bbfddff4210b20432b2257c73e2
bahae3 <bahaeddiine12@gmail.com>
2024-03-02 23:48:30 +0100

    I made the Transfer process work

--- main.py
+++ main.py
@@ -297,12 +297,13 @@ def clientInterface():
         benef_id = request.form.get('transfer_select')
         current_client = Client.query.get(current_user.client_id)
         client_to_have_money = Client.query.get(benef_id)
-
         amount = float(form_transfer.amount.data)
-        if amount < current_client.balance:
+        if amount <= current_client.balance:
             current_client.balance -= amount
             client_to_have_money.balance += amount
             db.session.commit()
+        else:
+            flash("You don't have enough money!")
 
     ## Deposit money section
     if form_deposit.validate_on_submit():
