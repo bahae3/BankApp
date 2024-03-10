3d4f18d533c3f6a840dced51ff08db0b31788ce0
bahae3 <bahaeddiine12@gmail.com>
2024-03-10 14:44:52 +0000

    I created new file to separate files and made some changes

--- static/client/addBenef.js
+++ static/client/addBenef.js
@@ -1,39 +1,3 @@
-// this is for account section
-const change_btn = document.querySelector(".change-password");
-const close_btn = document.querySelector(".close-btn-account");
-
-const acc_info = document.querySelector(".account-form");
-const change_password_field = document.querySelector(".account-change-password");
-
-const input_field = document.querySelectorAll(".account-input");
-const input_submit = document.querySelector(".account-update");
-
-
-const open_change_pass = (event) => {
-    event.preventDefault();
-    acc_info.style.filter = "blur(1.5px)";
-    change_btn.style.filter = "blur(1.5px)";
-    change_password_field.style.display = "block";
-    input_field.forEach(element => {
-        element.style.pointerEvents = "none";
-    });
-    input_submit.style.pointerEvents = "none";
-};
-
-const hide_change_pass = () => {
-    acc_info.style.filter = "blur(0)";
-    change_btn.style.filter = "blur(0)";
-    change_password_field.style.display = "none";
-    input_field.forEach(element => {
-        element.style.pointerEvents = "auto";
-    });
-    input_submit.style.pointerEvents = "auto";
-};
-
-change_btn.addEventListener("click", open_change_pass);
-close_btn.addEventListener("click", hide_change_pass);
-
-
 // this is for beneficiary section
 const add_btn = document.querySelector(".benef-add");
 const hide_btn = document.querySelector(".close-btn");
